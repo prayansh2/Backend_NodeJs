d187ac62519c4807b5594f6e1ecfcb85b02df30b
Priyanshu Saini <158676648+prayansh2@users.noreply.github.com>
2025-02-19 18:15:20 +0530

    step-1

---
+++ step-1/app.js
@@ -0,0 +1,15 @@
+//what is Node js
+//--->Node.js is a runtime environment that allows you to run JavaScript outside the browser. 
+
+
+// in step-1 first install the Node js 
+
+//creating some functions 
+
+function add(a,b)
+{
+    return a+b;
+}
+
+const a=add(1,3)
+console.log(a); //output is 4
