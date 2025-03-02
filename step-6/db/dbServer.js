32f327389e2df27120b7eb56e7dbe33ffb01613c
Priyanshu Saini <158676648+prayansh2@users.noreply.github.com>
2025-03-02 22:08:30 +0530

    Update dbServer.js

--- step-6/db/dbServer.js
+++ step-6/db/dbServer.js
@@ -1,11 +1,7 @@
 const mongoose=require("mongoose")
 
-
-
 const connectMongo= ()=>{
-   return mongoose.connect('mongodb://127.0.0.1/CourseDatabase')
-    
-   
+   return mongoose.connect('mongodb://127.0.0.1/CourseDatabase')   
 }
 
 module.exports=connectMongo;
